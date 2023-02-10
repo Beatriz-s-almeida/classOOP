7f2f9e081c86aae612314458bbb687a0d0397559
Beatriz Almeida <102378893+Beatriz-s-almeida@users.noreply.github.com>
2023-02-10 01:24:36 +0000

    aula1

---
+++ aula1ex2.java
@@ -0,0 +1,9 @@
+public class aula1ex2 {    
+    public static void main(String[] args){
+        double n1 = 8.7;
+        double n2 = 9.0;
+        double media = (n1+n2)/2;
+        System.out.println("A media é");
+        System.out.println(media);
+    }
+}
