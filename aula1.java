c6f8f54e8f32230c3be0da4184a887b63ddccfb3
Beatriz Almeida <102378893+Beatriz-s-almeida@users.noreply.github.com>
2023-02-10 01:19:01 +0000

    aula1

--- aula1.java
+++ aula1.java
@@ -1,5 +1,7 @@
 public class aula1{
     public static void main(String[] args){
-        
+        for(int contador = 0; contador < 100; contador++){
+            System.out.println(contador);
+        }
     }
 }
---
+++ aula1ex1.java
@@ -0,0 +1,7 @@
+import java.text.NumberFormat.Style;
+
+public class aula1ex1 {
+    public static void main(String[] args){
+        System.out.println("Beatriz Almeida");
+    }
+}
